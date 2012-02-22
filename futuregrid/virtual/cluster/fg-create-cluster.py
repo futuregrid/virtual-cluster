da90ef650e2efae26cf72bf541e14b2eb2851a30
Xiuwen Yang <xiuwyang@i136.(none)>
2012-02-22 16:51:16 -0500

    add depoly slurm to fg-create-cluster (not tested)

--- futuregrid/virtual/cluster/fg-create-cluster.py
+++ futuregrid/virtual/cluster/fg-create-cluster.py
@@ -1,9 +1,18 @@
 #! /usr/bin/env python
+import StringIO
+import ConfigParser
 import socket, time, getopt, sys, os
 
 class FgCreate:
-        def __init__(self):
-		pass
+
+	userkey=number=image=name=size=None
+
+        def __init__(self, userkey, number, image, name, size='m1.small'):
+		self.userkey = userkey
+		self.number = number
+		self.image = image
+		self.name = name
+		self.size= size			
 
 	def _run(command):
 	        return os.popen(command)
@@ -15,55 +24,137 @@ class FgCreate:
                                 sk.settimeout(1)
                                 sk.connect((ip, port))
                                 sk.close()
-                                print 'Ready to deploy'
+                                print 'Ready to deploy...'
                                 break
                         except Exception:
-                                print 'not ready to deploy yet'
+                                print 'Waitting VMs ready to deploy...'
                                 time.sleep(2)
 
-        def create_cluster(self, userkey, number, image, name, size='m1.small'):
-		print 'Creating virtual cluster...'
-		print 'name   -- ', name
-		print '#nodes -- ', number
-		print 'size   -- ', size
-		print 'image  -- ', image		
+        def create_cluster(self):
+		print '\n...Creating virtual cluster......'
+		print 'name   -- ', self.name
+		print '#nodes -- ', self.number
+		print 'size   -- ', self.size
+		print 'image  -- ', self.image
+		print '\n'		
 		
-		os.makedirs("futuregrid/cluster/%s" %name)
-		os.chdir("futuregrid/cluster/%s" %name)
+#		try:
+#			os.makedirs("futuregrid/cluster/%s" %self.name)
+#			os.chdir("futuregrid/cluster/%s" %self.name)
+#		except Exception:
+#			print "Creating directory futuregrid/cluster/%s falied. Cluster name is in use?" %self.name
+#			sys.exit()
 
-		cluster_size = int(number)+1
-#		print "euca-run-instances -k %s -n %d -t %s %s"  %(userkey, cluster_size, size, image)		
-		os.system("euca-run-instances -k %s -n %d -t %s %s"  %(userkey, cluster_size, size, image))
-#		result=os.popen("euca-run-instances -k %s.pem -n %d -t %s %s"  %(userkey, int(number), size, image)).read()
+		cluster_size = int(self.number)+1
+		os.system("euca-run-instances -k %s -n %d -t %s %s"  %(self.userkey, cluster_size, self.size, self.image))
 		os.system("euca-describe-instances|awk {'if ($2 ~ /^i/) print $2'}|sort|tail -n%d > instance_id.tmp" %cluster_size) 
 		os.system("euca-describe-addresses |grep -v 'i' |cut -f2 |sort |head -n%d > instance_ip.tmp" %cluster_size)
 		os.system("euca-describe-instances|awk {'if ($2 ~ /^i/) print $2,$3'}|sort|tail -n%d|awk {'print $2'} > image_id.tmp" %cluster_size)
 		os.system("euca-describe-instances|awk {'if ($2 ~ /^i/) print $2,$5'}|sort|tail -n%d|awk {'print $2'} > inner_ip.tmp" %cluster_size)
 		os.system("paste instance_id.tmp instance_ip.tmp image_id.tmp inner_ip.tmp> my_instances_list.txt")
-	
+		
+		print '...Associating public IPs......'
 		f = file('my_instances_list.txt')	
 		while True:
 			line = f.readline()
-			os.system("euca-associate-address -i `echo %s|awk {'print $1'}` `echo %s|awk {'print $2'}`" %(line, line))
-    			if len(line) == 0: 
+			if len(line) == 0:
 				break
+			line = [x for x in line.split()]
+			os.system("euca-associate-address -i %s %s" %(line[0], line[1]))
+
+	def clean(self):
+		print '...Clearing up......'
+		os.remove('instance_id.tmp')
+		os.remove('instance_ip.tmp')
+		os.remove('image_id.tmp')
+		os.remove('inner_ip.tmp')
+		print '...Done......'
+
+	def deploy_slurm(self):
+		print '\n...Deploying SLURM system......'
+
+		line_num = 0
+		control_node = None
+		compute_node_list = []
+
+		f = file('my_instances_list.txt')
+		while True:
+			line = f.readline()
+			if len(line) == 0:
+				break
+			line = [x for x in line.split()]
+#			os.system("ssh -i %s.pem -n ubuntu@%s 'sudo apt-get update'" %(self.userkey, line[1]))
+#			os.system("ssh -i %s.pem -n ubuntu@%s 'sudo apt-get install --yes slurm-llnl'" %(self.userkey, line[1]))
 		f.close()
 
-	def clean(self, name):
-         	os.chdir("futuregrid/cluster/%s" %name)
-		os.remove('rm instance_id.tmp')
-		os.remove('rm instance_ip.tmp')
-		os.remove('rm image_id.tmp')
-		os.remove('rm inner_ip.tmp')
-
-#	def deploy_slurm():
+#		cp = ConfigParser.ConfigParser()
 		
+#		config = StringIO.StringIO()
+#		config.write('[dummysection]')
+#		config.write(open('slurm.conf', 'r').read())
+#		config.seek(0, os.SEEK_SET)
+
+#		cp.readfp(config)
+#		cp.set("dummysection","controlmachine", "aa")
+#		cp.write(open('slurm.conf', 'w'))
+		
+#		os.system("sed -i '1d' slurm.conf")
+		
+		# add compute nodes info
+		f = file('my_instances_list.txt')
+		conf_file = open('slurm.conf', 'a')
+		while True:
+			line = f.readline()
+			if len(line) == 0:
+				break
+			line_num = line_num + 1
+			line = [x for x in line.split()]
+			if line_num == 1:
+				control_node = line
+				continue
+			compute_node_list.append(line)
+#			line = [x for x in line.split()]
+			conf_file.write("NodeName=%s Procs=1 State=UNKNOWN\n" %line[0])
+			conf_file.write("PartitionName=debug Nodes=%s Default=YES MaxTime=INFINITE State=UP\n" %line[0])
+		f.close()
+
+		# copy slurm.conf to control node
+		os.system("scp -i %s.pem slurm.conf ubuntu@%s:~/" %(self.userkey, control_node[1]))
+		os.system("ssh -i %s.pem -n ubuntu@%s 'sudo cp slurm.conf /etc/slurm-llnl/slurm.conf'" %(self.userkey, control_node[1]))
+
+		# genreate munge key on control node and copy it to compute nodes
+		# copy slurm.conf to every compute node
+		# print control_node, compute_node_list		
+		os.system("ssh -i %s.pem -n ubuntu@%s 'sudo /usr/sbin/create-munge-key'" %(self.userkey, control_node[1]))
+                os.system("ssh -i %s.pem -n ubuntu@%s 'sudo cat /etc/munge/munge.key' > munge.key" %(self.userkey, control_node[1]))
+		for nodes in compute_node_list:
+			os.system("scp -i %s.pem munge.key ubuntu@%s:~/" %(self.userkey, nodes[1]))
+			os.system("ssh -i %s.pem -n ubuntu@%s 'sudo cp munge.key /etc/munge/munge.key'" %(self.userkey, nodes[1]))
+			os.system("ssh -i %s.pem -n ubuntu@%s 'sudo chown munge /etc/munge/munge.key'" %(self.userkey, nodes[1]))
+			os.system("ssh -i %s.pem -n ubuntu@%s 'sudo chgrp munge /etc/munge/munge.key'" %(self.userkey, nodes[1]))
+			os.system("ssh -i %s.pem -n ubuntu@%s 'sudo chmod 400 /etc/munge/munge.key'" %(self.userkey, nodes[1]))
+			os.system("scp -i %s.pem slurm.conf ubuntu@%s:~/" %(self.userkey, nodes[1]))
+			os.system("ssh -i %s.pem -n ubuntu@%s 'sudo cp slurm.conf /etc/slurm-llnl/slurm.conf'" %(self.userkey, nodes[1]))
+
+		print '\n...Starting SLURM system......\n'
+		# run slurm on control node
+		os.system("ssh -i %s.pem -n ubuntu@%s 'sudo /etc/init.d/slurm-llnl start'" %(self.userkey, control_node[1]))
+		os.system("ssh -i $1.pem -n ubuntu@%s 'sudo /etc/init.d/munge start'" %(self.userkey, control_node[1]))
+		
+		# run slurm on compute node
+		for nodes in compute_node_list:
+			os.system("ssh -i %s.pem -n ubuntu@%s 'sudo /etc/init.d/slurm-llnl start'" %(self.userkey, nodes[1]))
+	                os.system("ssh -i $1.pem -n ubuntu@%s 'sudo /etc/init.d/munge start'" %(self.userkey, nodes[1]))
+
+		
+
 
 def usage():
         print '-h/--help    Display this help.'
         print '-u/--userkey provide userkey'
         print '-n/--number  provide number of nodes(control node not included)'
         print '-s/--size    provide size of cluster(control node not included) default: s1.small'
+	print '-a/--name    provide name of virtual cluster.' 
 
 def main():
 	userkey=number=image=size=name=None
@@ -89,14 +180,14 @@ def main():
 		elif opt in ("-a", "--name"):
 			name=arg
             						
-        fgc=FgCreate()
-	
 	if size == None:
-	        fgc.create_cluster(userkey, number, image, name)
+	        fgc=FgCreate(userkey, number, image, name)
 	else:
-		fgc.create_cluster(userkey, number, image, name, size)
+		fgc=FgCreate(userkey, number, image, name, size)
 
-	fgc.clean(name)
+	fgc.create_cluster()
+	fgc.deploy_slurm()
+	fgc.clean()
 
 if __name__ == '__main__':
     main()
