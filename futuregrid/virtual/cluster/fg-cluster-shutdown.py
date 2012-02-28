46363eced670e1343a4615b75c6c088c7774fd7c
Xiuwen Yang <xiuwyang@i136.(none)>
2012-02-27 22:33:26 -0500

    added fg-cluster-shutdown

--- futuregrid/virtual/cluster/fg-cluster-shutdown.py
+++ futuregrid/virtual/cluster/fg-cluster-shutdown.py
@@ -1,27 +1,74 @@
 #! /usr/bin/env python
-import socket, time, getopt, sys, os
+import getopt, sys, os, pickle
+
+class CloudInstances:
+
+	cloud_instances = []
+	
+	def __init__(self, name):
+		self.clear()
+		if self.check_name(name):
+			self.cloud_instances = self.load(name)
+		else:
+			print 'Error in finding virtual cluster. Not created?'
+			sys.exit()
+		return
+
+	def list(self):
+		return self.cloud_instances
+
+	def clear(self):
+		self.cloud_instances = []
+
+	def check_name(self, name):
+		try:
+			f = open("cloud_instances.dat", "r")
+			cloud_list = pickle.load(f)
+			for cloud in cloud_list:	
+				if cloud[0]['name'] == name:
+					return True
+			return False
+		except:
+			return False
+			
+
+	def load(self, name):
+		f = open("cloud_instances.dat", "r")
+		cloud_list = pickle.load(f)
+		for cloud in cloud_list:
+			if cloud[0]['name'] == name:
+				return cloud
+
+	def del_by_name(self, name):
+		f = open("cloud_instances.dat", "r")
+		cloud_list = pickle.load(f)
+		for cloud in cloud_list:
+			if cloud[0]['name'] == name:
+				cloud_list.remove(cloud)
+				f = open("cloud_instances.dat", "w")
+				pickle.dump(cloud_list, f)
+				f.close()
+				return
 
 class FgShutdown:
 
         def __init__(self, name):
                 self.name = name
+		self.cloud_instances = CloudInstances(name)
 
-	def clean(self):
-                print '...Clearing up......'
-		os.remove('my_instances_list.txt')
-                print '...Done......'
+	def clean(self, name):
+		print '...Clearing up......'
+		self.cloud_instances.del_by_name(name)       
+	        print '...Done......'
+	
+	def terminate_instance(self, instance_id):
+		print 'terminating instance %s' %instance_id
+		os.system("euca-terminate-instances %s" %instance_id)
 
 	def shut_down(self):
-		f = file('my_instances_list.txt')
-                while True:
-                        line = f.readline()
-                        if len(line) == 0:
-                                break
-                        line = [x for x in line.split()]
-			print "...Shutting down %s" %line[0]
-			os.system("euca-terminate-instances %s" %line[0])
-                f.close()
-
+		for instance in self.cloud_instances.list()[1:]:
+			self.terminate_instance(instance['id'])
+		self.clean(self.name)
 
 def usage():
         print '-h/--help    Display this help.'
@@ -45,7 +92,6 @@ def main():
 
         fgs=FgShutdown(name)
 	fgs.shut_down()
-	fgs.clean()
 
 if __name__ == '__main__':
     main()
