87b4e2f0817354739759f08647f95e47dc1fa963
Gregor von Laszewski <laszewski@gmail.com>
2012-03-16 16:40:09 -0400

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ program is included to test the functionality. of teh virtual cluster.
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1.6'
+version = '0.1.7'
 
 classifiers = """\
 Intended Audience :: Developers
