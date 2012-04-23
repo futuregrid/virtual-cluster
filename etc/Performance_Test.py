1a8b4d984233f8a5573c2703912d60c27301aacd
Gregor von Laszewski <laszewski@gmail.com>
2012-04-23 13:43:45 -0400

    fixed test script problem

--- etc/Performance_Test.py
+++ etc/Performance_Test.py
@@ -3,19 +3,19 @@
 
 import os
 
-
 ######################################
 #change here to parameterize the test#
 ######################################
 test_runs = 3
 instance_type = 'm1.small'
 nodes_num = 2
+image_id = 'ami-0000001d'
+
 
 def process_data(create_time, run_prog, terminate_time):
-    if not create_time.split()[0] == 'Performance data:':
+    if not create_time.split()[0] == 'Performance':
         print 'Test failed'
         return
-
     with open('performance_test', 'aw') as pt:
         pt.write(create_time+'\t'+run_prog+'\t'+terminate_time+'\n')
     pt.close()
@@ -26,21 +26,26 @@ def performance_test():
         print '\n\nRunning Test %d' % int(i + 1)
         print '\n\nCreating cluster'
         create_time =  create_cluster(instance_type, nodes_num)
+        print create_time
         print '\n\nRunning MPI program'
         run_prog =  run_program(nodes_num)
+        print run_prog
         print '\n\nTerminating cluster'
         terminate_time =  terminate_cluster()
+        print terminate_time
         print 'Adding data'
         process_data(create_time, run_prog, terminate_time)
 
 def create_cluster(instance_type, number):
-    return os.popen("fg-cluster run -a test -i ami-0000001d -t %s -n %s" % (instance_type, number)).read().split('\n')[-2]
+    print '\nInstance type -- %s' % instance_type
+    print 'Number of computation nodes -- %s' % number
+    return os.popen("python FGCluster.py run -a test -i %s -t %s -n %s" % (image_id, instance_type, number)).read().split('\n')[-2]
 
 def terminate_cluster():
-    return os.popen("fg-cluster terminate -a test").read().split('\n')[-2]
+    return os.popen("python FGCluster.py terminate -a test").read().split('\n')[-2]
 
 def run_program(number):
-    return os.popen("fg-cluster mpirun -p helloworld.c -a test -n %s" % number).read().split('\n')[-2]
+    return os.popen("python FGCluster.py mpirun -p helloworld.c -a test -n %s" % number).read().split('\n')[-2]
 
     
 def main():
