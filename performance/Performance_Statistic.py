fe1fe506c7789cde37aef0072b08311a53df5a23
Gregor von Laszewski <laszewski@gmail.com>
2012-04-26 16:18:39 -0400

    resolved format issue

--- performance/Performance_Statistic.py
+++ performance/Performance_Statistic.py
@@ -15,46 +15,47 @@ class Performance_Static():
 
     def process_file(self, args):
         with open(os.path.expanduser(args.file)) as raw_input:
-            for line in raw_input:
-                values = line.split('\n')[0].split('\t')
-                # if nova data
-                if len(values) == self.nova_data_length:
-                    if not values[1] in self.nova_data:
-                        self.nova_data[values[1]] = {}
-                        self.nova_data[values[1]]['t_total'] = [float(values[2])]
-                        self.nova_data[values[1]]['t_setup_getip'] = [float(values[3])]
-                        self.nova_data[values[1]]['t_setup_install'] = [float(values[4])]
-                        self.nova_data[values[1]]['t_setup_configure'] = [float(values[5])]
-                        self.nova_data[values[1]]['t_ipfail'] = [float(values[6])]
-                        self.nova_data[values[1]]['t_ipchange'] = [float(values[7])]
-                        self.nova_data[values[1]]['t_termination'] = [float(values[8])]
-                        self.nova_data[values[1]]['t_execute'] = [float(values[9])]
-                        self.nova_data[values[1]]['t_shutdown'] = [float(values[10])]
-                    else:
-                        self.nova_data[values[1]]['t_total'].append(float(values[2]))
-                        self.nova_data[values[1]]['t_setup_getip'].append(float(values[3]))
-                        self.nova_data[values[1]]['t_setup_install'].append(float(values[4]))
-                        self.nova_data[values[1]]['t_setup_configure'].append(float(values[5]))
-                        self.nova_data[values[1]]['t_ipfail'].append(float(values[6]))
-                        self.nova_data[values[1]]['t_ipchange'].append(float(values[7]))
-                        self.nova_data[values[1]]['t_termination'].append(float(values[8]))
-                        self.nova_data[values[1]]['t_execute'].append(float(values[9]))
-                        self.nova_data[values[1]]['t_shutdown'].append(float(values[10]))
-                # if euca_data
-                if len(values) == self.euca_data_length:
-                    if not values[1] in self.euca_data:
-                        self.euca_data[values[1]] = {}
-                        self.euca_data[values[1]]['t_total'] = [float(values[2])]
-                        self.euca_data[values[1]]['t_setup_install'] = [float(values[3])]
-                        self.euca_data[values[1]]['t_setup_configure'] = [float(values[4])]
-                        self.euca_data[values[1]]['t_execute'] = [float(values[5])]
-                        self.euca_data[values[1]]['t_shutdown'] = [float(values[6])]
-                    else:
-                        self.euca_data[values[1]]['t_total'].append(float(values[2]))
-                        self.euca_data[values[1]]['t_setup_install'].append(float(values[3]))
-                        self.euca_data[values[1]]['t_setup_configure'].append(float(values[4]))
-                        self.euca_data[values[1]]['t_execute'].append(float(values[5]))
-                        self.euca_data[values[1]]['t_shutdown'].append(float(values[6]))
+            content = raw_input.readlines()
+        for line in content:
+            values = line.split('\n')[0].split('\t')
+            # if nova data
+            if len(values) == self.nova_data_length:
+                if not values[1] in self.nova_data:
+                    self.nova_data[values[1]] = {}
+                    self.nova_data[values[1]]['t_total'] = [float(values[2])]
+                    self.nova_data[values[1]]['t_setup_getip'] = [float(values[3])]
+                    self.nova_data[values[1]]['t_setup_install'] = [float(values[4])]
+                    self.nova_data[values[1]]['t_setup_configure'] = [float(values[5])]
+                    self.nova_data[values[1]]['t_ipfail'] = [float(values[6])]
+                    self.nova_data[values[1]]['t_ipchange'] = [float(values[7])]
+                    self.nova_data[values[1]]['t_termination'] = [float(values[8])]
+                    self.nova_data[values[1]]['t_execute'] = [float(values[9])]
+                    self.nova_data[values[1]]['t_shutdown'] = [float(values[10])]
+                else:
+                    self.nova_data[values[1]]['t_total'].append(float(values[2]))
+                    self.nova_data[values[1]]['t_setup_getip'].append(float(values[3]))
+                    self.nova_data[values[1]]['t_setup_install'].append(float(values[4]))
+                    self.nova_data[values[1]]['t_setup_configure'].append(float(values[5]))
+                    self.nova_data[values[1]]['t_ipfail'].append(float(values[6]))
+                    self.nova_data[values[1]]['t_ipchange'].append(float(values[7]))
+                    self.nova_data[values[1]]['t_termination'].append(float(values[8]))
+                    self.nova_data[values[1]]['t_execute'].append(float(values[9]))
+                    self.nova_data[values[1]]['t_shutdown'].append(float(values[10]))
+            # if euca_data
+            if len(values) == self.euca_data_length:
+                if not values[1] in self.euca_data:
+                    self.euca_data[values[1]] = {}
+                    self.euca_data[values[1]]['t_total'] = [float(values[2])]
+                    self.euca_data[values[1]]['t_setup_install'] = [float(values[3])]
+                    self.euca_data[values[1]]['t_setup_configure'] = [float(values[4])]
+                    self.euca_data[values[1]]['t_execute'] = [float(values[5])]
+                    self.euca_data[values[1]]['t_shutdown'] = [float(values[6])]
+                else:
+                    self.euca_data[values[1]]['t_total'].append(float(values[2]))
+                    self.euca_data[values[1]]['t_setup_install'].append(float(values[3]))
+                    self.euca_data[values[1]]['t_setup_configure'].append(float(values[4]))
+                    self.euca_data[values[1]]['t_execute'].append(float(values[5]))
+                    self.euca_data[values[1]]['t_shutdown'].append(float(values[6]))
         self.process_data_nova()
         self.process_data_euca()
 
@@ -62,45 +63,129 @@ class Performance_Static():
 
         writer = csv.writer(file('performance_data_nova.csv', 'wb'))
         writer.writerow(['name',
-                         't_total_avg', 't_total_min', 't_total_max', 't_total_stdev',
-                         't_setup_getip_avg', 't_setup_getip_min', 't_setup_getip_max', 't_setup_getip_stdev',
-                         't_setup_install_avg', 't_setup_install_min', 't_setup_install_max', 't_setup_install_stdev',
-                         't_setup_configure_avg', 't_setup_configure_min', 't_setup_configure_max', 't_setup_configure_stdev',
-                         't_ipfail_avg', 't_ipfail_min', 't_ipfail_max', 't_ipfail_stdev',
-                         't_ipchange_avg', 't_ipchange_min', 't_ipchange_max', 't_ipchange_stdev',
-                         't_termination_avg', 't_termination_min', 't_termination_max', 't_termination_stdev',
-                         't_execute_avg', 't_execute_min', 't_execute_max', 't_execute_stdev',
-                         't_shutdown_avg', 't_shutdown_min', 't_shutdown_max', 't_shutdown_stdev'])
+                         't_total_avg',
+                         't_total_min',
+                         't_total_max',
+                         't_total_stdev',
+                         't_setup_getip_avg',
+                         't_setup_getip_min',
+                         't_setup_getip_max',
+                         't_setup_getip_stdev',
+                         't_setup_install_avg',
+                         't_setup_install_min',
+                         't_setup_install_max',
+                         't_setup_install_stdev',
+                         't_setup_configure_avg',
+                         't_setup_configure_min',
+                         't_setup_configure_max',
+                         't_setup_configure_stdev',
+                         't_ipfail_avg',
+                         't_ipfail_min',
+                         't_ipfail_max',
+                         't_ipfail_stdev',
+                         't_ipchange_avg',
+                         't_ipchange_min',
+                         't_ipchange_max',
+                         't_ipchange_stdev',
+                         't_termination_avg',
+                         't_termination_min',
+                         't_termination_max',
+                         't_termination_stdev',
+                         't_execute_avg',
+                         't_execute_min',
+                         't_execute_max',
+                         't_execute_stdev',
+                         't_shutdown_avg',
+                         't_shutdown_min',
+                         't_shutdown_max',
+                         't_shutdown_stdev'])
 
         for performance_name, data in self.nova_data.items():
             writer.writerow([performance_name,
-                             average(array(data['t_total'])), array(data['t_total']).min(), array(data['t_total']).max(), std(array(data['t_total'])),
-                             average(array(data['t_setup_getip'])), array(data['t_setup_getip']).min(), array(data['t_setup_getip']).max(), std(array(data['t_setup_getip'])),
-                             average(array(data['t_setup_install'])), array(data['t_setup_install']).min(), array(data['t_setup_install']).max(), std(array(data['t_setup_install'])),
-                             average(array(data['t_setup_configure'])), array(data['t_setup_configure']).min(), array(data['t_setup_configure']).max(), std(array(data['t_setup_configure'])),
-                             average(array(data['t_ipfail'])), array(data['t_ipfail']).min(), array(data['t_ipfail']).max(), std(array(data['t_ipfail'])),
-                             average(array(data['t_ipchange'])), array(data['t_ipchange']).min(), array(data['t_ipchange']).max(), std(array(data['t_ipchange'])),
-                             average(array(data['t_termination'])), array(data['t_termination']).min(), array(data['t_termination']).max(), std(array(data['t_termination'])),
-                             average(array(data['t_execute'])), array(data['t_execute']).min(), array(data['t_execute']).max(), std(array(data['t_execute'])),
-                             average(array(data['t_shutdown'])), array(data['t_shutdown']).min(), array(data['t_shutdown']).max(), std(array(data['t_shutdown'])),
+                             average(array(data['t_total'])),
+                             array(data['t_total']).min(),
+                             array(data['t_total']).max(),
+                             std(array(data['t_total'])),
+                             average(array(data['t_setup_getip'])),
+                             array(data['t_setup_getip']).min(),
+                             array(data['t_setup_getip']).max(),
+                             std(array(data['t_setup_getip'])),
+                             average(array(data['t_setup_install'])),
+                             array(data['t_setup_install']).min(),
+                             array(data['t_setup_install']).max(),
+                             std(array(data['t_setup_install'])),
+                             average(array(data['t_setup_configure'])),
+                             array(data['t_setup_configure']).min(),
+                             array(data['t_setup_configure']).max(),
+                             std(array(data['t_setup_configure'])),
+                             average(array(data['t_ipfail'])),
+                             array(data['t_ipfail']).min(),
+                             array(data['t_ipfail']).max(),
+                             std(array(data['t_ipfail'])),
+                             average(array(data['t_ipchange'])),
+                             array(data['t_ipchange']).min(),
+                             array(data['t_ipchange']).max(),
+                             std(array(data['t_ipchange'])),
+                             average(array(data['t_termination'])),
+                             array(data['t_termination']).min(),
+                             array(data['t_termination']).max(),
+                             std(array(data['t_termination'])),
+                             average(array(data['t_execute'])), 
+                             array(data['t_execute']).min(),
+                             array(data['t_execute']).max(),
+                             std(array(data['t_execute'])),
+                             average(array(data['t_shutdown'])),
+                             array(data['t_shutdown']).min(),
+                             array(data['t_shutdown']).max(),
+                             std(array(data['t_shutdown'])),
                              ])
 
     def process_data_euca(self):
         writer = csv.writer(file('performance_data_euca.csv', 'wb'))
         writer.writerow(['name',
-                         't_total_avg', 't_total_min', 't_total_max', 't_total_stdev',
-                         't_setup_install_avg', 't_setup_install_min', 't_setup_install_max', 't_setup_install_stdev',
-                         't_setup_configure_avg', 't_setup_configure_min', 't_setup_configure_max', 't_setup_configure_stdev',
-                         't_execute_avg', 't_execute_min', 't_execute_max', 't_execute_stdev',
-                         't_shutdown_avg', 't_shutdown_min', 't_shutdown_max', 't_shutdown_stdev'])
+                         't_total_avg',
+                         't_total_min',
+                         't_total_max',
+                         't_total_stdev',
+                         't_setup_install_avg',
+                         't_setup_install_min',
+                         't_setup_install_max',
+                         't_setup_install_stdev',
+                         't_setup_configure_avg',
+                         't_setup_configure_min',
+                         't_setup_configure_max',
+                         't_setup_configure_stdev',
+                         't_execute_avg',
+                         't_execute_min',
+                         't_execute_max',
+                         't_execute_stdev',
+                         't_shutdown_avg',
+                         't_shutdown_min',
+                         't_shutdown_max',
+                         't_shutdown_stdev'])
 
         for performance_name, data in self.euca_data.items():
             writer.writerow([performance_name,
-                             average(array(data['t_total'])), array(data['t_total']).min(), array(data['t_total']).max(), std(array(data['t_total'])),
-                             average(array(data['t_setup_install'])), array(data['t_setup_install']).min(), array(data['t_setup_install']).max(), std(array(data['t_setup_install'])),
-                             average(array(data['t_setup_configure'])), array(data['t_setup_configure']).min(), array(data['t_setup_configure']).max(), std(array(data['t_setup_configure'])),
-                             average(array(data['t_execute'])), array(data['t_execute']).min(), array(data['t_execute']).max(), std(array(data['t_execute'])),
-                             average(array(data['t_shutdown'])), array(data['t_shutdown']).min(), array(data['t_shutdown']).max(), std(array(data['t_shutdown'])),
+                             average(array(data['t_total'])),
+                             array(data['t_total']).min(),
+                             array(data['t_total']).max(),
+                             std(array(data['t_total'])),
+                             average(array(data['t_setup_install'])),
+                             array(data['t_setup_install']).min(),
+                             array(data['t_setup_install']).max(),
+                             std(array(data['t_setup_install'])),
+                             average(array(data['t_setup_configure'])),
+                             array(data['t_setup_configure']).min(),
+                             array(data['t_setup_configure']).max(),
+                             std(array(data['t_setup_configure'])),
+                             average(array(data['t_execute'])),
+                             array(data['t_execute']).min(),
+                             array(data['t_execute']).max(),
+                             std(array(data['t_execute'])),
+                             average(array(data['t_shutdown'])),
+                             array(data['t_shutdown']).min(),
+                             array(data['t_shutdown']).max(),
+                             std(array(data['t_shutdown'])),
                              ])
 
 def main():
--- performance/Performance_Testall.py
+++ performance/Performance_Testall.py
@@ -9,97 +9,54 @@ import ConfigParser
 ######################################
 '''
 This is Nova setting
-If want to test performance in eucalyputs, need to change futuregrid.cfg and instance type and image id which should be ubuntu natty
+If want to test performance in eucalyputs, need to change futuregrid.cfg accordingly
+Image shoudld be ubuntu natty if u want to use IU ubuntu repository
 '''
 test_runs = 3
-nodes_num_1 = 1
-nodes_num_2 = 2
-nodes_num_4 = 4
-nodes_num_8 = 8
-nodes_num_16 = 16
-nodes_num_24 = 24
-nodes_num_32 = 32
 node_nums = [1,2,4,8,16,24,32]
-image_id = None
-instance_type_small = None
-instance_type_medium = None
-instance_type_large = None
-
-def read_config():
-    global image_id
-    global instance_type_small
-    global instance_type_medium
-    global instance_type_large
-
-    config = ConfigParser.ConfigParser()
-    config.read([os.path.expanduser('~/.futuregrid/futuregrid.cfg'),
-                         'futuregrid.cfg'])
-    cloud = config.get('virtual-cluster', 'cloud')
-    if cloud == 'nova':
-        image_id = 'ami-0000001d'
-        instance_type_small = 'm1.small'
-        instance_type_medium = 'm1.medium'
-        instance_type_large = 'm1.large'
-
-    elif cloud == 'eucalyptus':
-        image_id = 'emi-FC6A1197'
-        instance_type_small = 'm1.small'
-        instance_type_medium = 'c1.medium'
-        instance_type_large = 'm1.large'
+cloud_specific_para = {'nova':{'image_id':'ami-0000001d', 'instance_type':['m1.small', 'm1.medium', 'm1.large']}, 
+                       'eucalyptus':{'image_id':'emi-FC6A1197', 'instance_type':['m1.small', 'c1.medium', 'm1.large']}}
 
 def process_data(create_time, run_prog, terminate_time):
     if not create_time.split()[0] == 'Performance':
         print 'Test failed'
         return
-    with open('performance_test', 'aw') as pt:
+    with open('performance_test_raw', 'a') as pt:
         pt.write(create_time+'\t'+run_prog+'\t'+terminate_time+'\n')
     pt.close()
     
 def performance_test():
 
-    # instance type - m1.small
-    for node_num in node_nums:
-        for i in range(test_runs):
-            print '\n\nRunning Test %d' % int(i + 1)
-            print 'Node-num: %s\tInstance-type: %s' % (node_num, instance_type_small)
-            print '\n\nCreating cluster'
-            create_time =  create_cluster(instance_type_small, node_num)
-            print '\n\nRunning MPI program'
-            run_prog =  run_program(node_num)
-            print '\n\nTerminating cluster'
-            terminate_time =  terminate_cluster()
-            print 'Adding data'
-            process_data(create_time, run_prog, terminate_time)
+    config = ConfigParser.ConfigParser()
+    config.read([os.path.expanduser('~/.futuregrid/futuregrid.cfg'),
+                         'futuregrid.cfg'])
+    cloud = config.get('virtual-cluster', 'cloud')
+    image_id = cloud_specific_para[cloud]['image_id']
+    instance_types = cloud_specific_para[cloud]['instance_type']
 
-    # instance type - m1.medium
-    for node_num in node_nums:
-        for i in range(test_runs):
-            print '\n\nRunning Test %d' % int(i + 1)
-            print 'Node-num: %s\tInstance-type: %s' % (node_num, instance_type_medium)
-            print '\n\nCreating cluster'
-            create_time =  create_cluster(instance_type_medium, node_num)
-            print '\n\nRunning MPI program'
-            run_prog =  run_program(node_num)
-            print '\n\nTerminating cluster'
-            terminate_time =  terminate_cluster()
-            print 'Adding data'
-            process_data(create_time, run_prog, terminate_time)
+    # Writes the first row
+    if not os.path.isfile('performance_test_raw'):
+        with open('performance_test_raw', 'w') as pt:
+            pt.write('\t\t\tTest name\tTotal Time\tIP association time\tInstallation time'
+                     '\tConfiguration time\tIP association failure\tIP change\tTermination\tExecution time\tTermination\n')
+        pt.close()
 
-    # instance type - m1.large
-    for node_num in node_nums:
-        for i in range(test_runs):
-            print '\n\nRunning Test %d' % int(i + 1)
-            print 'Node-num: %s\tInstance-type: %s' % (node_num, instance_type_large)
-            print '\n\nCreating cluster'
-            create_time =  create_cluster(instance_type_large, node_num)
-            print '\n\nRunning MPI program'
-            run_prog =  run_program(node_num)
-            print '\n\nTerminating cluster'
-            terminate_time =  terminate_cluster()
-            print 'Adding data'
-            process_data(create_time, run_prog, terminate_time)
+    # Runs tests, writes data into file
+    for instance_type in instance_types:
+        for node_num in node_nums:
+            for i in range(test_runs):
+                print '\n\nRunning Test %d' % int(i + 1)
+                print 'Node-num: %s\tInstance-type: %s' % (node_num, instance_type)
+                print '\n\nCreating cluster'
+                create_time =  create_cluster(image_id, instance_type, node_num)
+                print '\n\nRunning MPI program'
+                run_prog =  run_program(node_num)
+                print '\n\nTerminating cluster'
+                terminate_time =  terminate_cluster()
+                print 'Adding data'
+                process_data(create_time, run_prog, terminate_time)
 
-def create_cluster(instance_type, number):
+def create_cluster(image_id, instance_type, number):
     print '\nInstance type -- %s' % instance_type
     print 'Number of computation nodes -- %s' % number
     return os.popen("fg-cluster run -a test -i %s -t %s -n %s" % (image_id, instance_type, number)).read().split('\n')[-2]
@@ -112,7 +69,6 @@ def run_program(number):
 
     
 def main():
-    read_config()
     performance_test()
 
 if __name__ == '__main__':
